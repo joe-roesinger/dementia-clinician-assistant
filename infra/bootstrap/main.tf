989df9b57263eb0013c895885943793b45a22373
Joe Roesinger <joe.roesinger@gmail.com>
2026-09-24 18:15:18 -0700

    feat(infra): bootstrap the terraform google state bucket
    - Put state in GCS so it persistent
    - Added verisoning and prevent_destroy to trakc changes and make it ACTUALLY persist

---
+++ infra/bootstrap/main.tf
@@ -0,0 +1,42 @@
+terraform {
+  required_version = "~> 1.16"
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 8.4"
+    }
+  }
+}
+
+provider "google" {
+  project = var.project
+  region  = var.region
+}
+
+resource "google_storage_bucket" "tfstate" {
+  name     = "${var.project}-tfstate"
+  location = var.region
+  project  = var.project
+
+  versioning {
+    enabled = true
+  }
+
+  uniform_bucket_level_access = true
+  public_access_prevention    = "enforced"
+  force_destroy               = false
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  lifecycle_rule {
+    condition {
+      num_newer_versions = 10
+    }
+
+    action {
+      type = "Delete"
+    }
+  }
+}
---
+++ infra/bootstrap/variables.tf
@@ -0,0 +1,5 @@
+variable "project" {}
+
+variable "region" {
+  default = "us-central1"
+}
